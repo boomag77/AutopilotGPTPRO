ed43ff624781f58cdef610ebfc4f7c89baae0dcf
boomag77 <96821826+boomag77@users.noreply.github.com>
2024-03-30 10:43:31 -0400

    Add CoreData entities. Classes NSManagedObject didn't generate yet

--- AutopilotGPTPRO/Model/DataModels.swift
+++ AutopilotGPTPRO/Model/DataModels.swift
@@ -1,7 +1,7 @@
 
 import Foundation
 
-enum MessageSender {
+enum MessageSender: Hashable {
     case user
     case server
 }
@@ -11,14 +11,25 @@ struct InstructionModel {
     var text: String
 }
 
-struct SessionModel {
+struct SessionModel: Equatable, Hashable {
     var date: Date
-    var messages: [MessageModel]
+    var messages: Set<MessageModel>
     var tokensUsed: Int?
 }
 
-struct MessageModel {
+struct MessageModel: Hashable {
+    
+    static func == (lhs: MessageModel, rhs: MessageModel) -> Bool {
+        return lhs.date == rhs.date &&
+                lhs.sender == rhs.sender &&
+                lhs.text == rhs.text &&
+                lhs.cost == rhs.cost &&
+                lhs.session == rhs.session
+    }
+    
     var date: Date
     var sender: MessageSender
     var text: String
+    var cost: Int
+    var session: SessionModel
 }
