997b06ed2473e3014ee4e9a71e690a035dcecd3c
boomag77 <96821826+boomag77@users.noreply.github.com>
2024-03-31 18:00:38 -0400

    Add swithcing between views in InstructionsController logic

--- AutopilotGPTPRO/Controller/InstructionsViewController.swift
+++ AutopilotGPTPRO/Controller/InstructionsViewController.swift
@@ -5,9 +5,23 @@ final class InstructionsViewController: UIViewController {
     
     private var instructions: [InstructionModel] = []
     
-    private lazy var screenTitle: UILabel = {
+    private lazy var controllerTitle: UILabel = {
         let label = ScreenTitleLabel(withText: "Instructions")
         return label
+    }()
+    
+    private lazy var listView: UIView = {
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var editOrCreateView: UIView = {
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     private lazy var addNewButton: UIButton = {
@@ -25,7 +39,7 @@ final class InstructionsViewController: UIViewController {
         button.titleLabel?.adjustsFontForContentSizeCategory = true
         button.clipsToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(self.addNewButtonPressed), for: .touchUpInside)
+        button.addTarget(self, action: #selector(self.addNewButtonTapped), for: .touchUpInside)
         return button
     }()
     
@@ -51,25 +65,67 @@ final class InstructionsViewController: UIViewController {
         
         view.backgroundColor = .systemGray6
         
-        view.addSubview(screenTitle)
-        view.addSubview(addNewButton)
-        view.addSubview(tableView)
+        view.addSubview(controllerTitle)
         
         NSLayoutConstraint.activate([
-            screenTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
-            screenTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
-            screenTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0),
+            controllerTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            controllerTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            controllerTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16.0)
+        ])
+        setupViews()
+        
+    }
+    
+    private func setupViews() {
+        
+        self.view.addSubview(listView)
+        self.view.insertSubview(editOrCreateView, at: 1)
+        
+        NSLayoutConstraint.activate([
+            listView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            listView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            listView.topAnchor.constraint(equalTo: controllerTitle.bottomAnchor),
+            listView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             
-            addNewButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
-            addNewButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
-            addNewButton.topAnchor.constraint(equalTo: screenTitle.bottomAnchor, constant: 10.0),
-            
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
-            tableView.topAnchor.constraint(equalTo: addNewButton.bottomAnchor, constant: 10.0),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16.0)
+            editOrCreateView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            editOrCreateView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            editOrCreateView.topAnchor.constraint(equalTo: controllerTitle.bottomAnchor),
+            editOrCreateView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
         
+        listView.addSubview(addNewButton)
+        listView.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            addNewButton.leadingAnchor.constraint(equalTo: listView.leadingAnchor, constant: 16.0),
+            addNewButton.trailingAnchor.constraint(equalTo: listView.trailingAnchor, constant: -16.0),
+            addNewButton.topAnchor.constraint(equalTo: listView.topAnchor, constant: 10.0),
+            
+            tableView.leadingAnchor.constraint(equalTo: listView.leadingAnchor, constant: 16.0),
+            tableView.trailingAnchor.constraint(equalTo: listView.trailingAnchor, constant: -16.0),
+            tableView.topAnchor.constraint(equalTo: addNewButton.bottomAnchor, constant: 10.0),
+            tableView.bottomAnchor.constraint(equalTo: listView.bottomAnchor, constant: -16.0)
+        ])
+        
+        let exitButton = UIButton(type: .system)
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        var config = UIButton.Configuration.filled()
+        config.title = "Back"
+        config.baseBackgroundColor = UIColor.systemBlue
+        config.baseForegroundColor = .white.withAlphaComponent(0.85)
+        config.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 20, bottom: 15, trailing: 20)
+        config.cornerStyle = .large
+        exitButton.configuration = config
+        exitButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        editOrCreateView.addSubview(exitButton)
+        NSLayoutConstraint.activate([
+            exitButton.centerXAnchor.constraint(equalTo: editOrCreateView.centerXAnchor),
+            exitButton.centerYAnchor.constraint(equalTo: editOrCreateView.centerYAnchor)
+        ])
+        
+        
+        editOrCreateView.isHidden = true
+        fetchData()
     }
     
     private func fetchData() {
@@ -77,8 +133,16 @@ final class InstructionsViewController: UIViewController {
         tableView.reloadData()
     }
     
-    @objc private func addNewButtonPressed() {
+    @objc private func addNewButtonTapped() {
         print("Add new Button pressed")
+        editOrCreateView.isHidden.toggle()
+        listView.isHidden.toggle()
+    }
+    
+    @objc private func backButtonTapped() {
+        editOrCreateView.isHidden.toggle()
+        listView.isHidden.toggle()
+        fetchData()
     }
 
 }
