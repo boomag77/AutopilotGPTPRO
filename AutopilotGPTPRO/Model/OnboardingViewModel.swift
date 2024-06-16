1910e19e6e5630761aec5a4fb51a71db9983f74f
boomag77 <96821826+boomag77@users.noreply.github.com>
2024-06-16 13:40:03 -0400

    fina

--- AutopilotGPTPRO/Model/OnboardingViewModel.swift
+++ AutopilotGPTPRO/Model/OnboardingViewModel.swift
@@ -29,7 +29,7 @@ class OnboardingViewModel {
             case .russian:
                 questionSet = russianQuestions
         }
-        self.currentQuestionIndex = 14
+        self.currentQuestionIndex = 0
     }
     
     func getQuestion() -> Question? {
